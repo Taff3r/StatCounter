5f87626c54a97b90f593dfb1e60f64930940a9e4
Simon <simon.tenggren@gmail.com>
2019-07-14 18:58:18 +0200

    Started model

--- app/src/main/java/taffer/statcounter/MainActivity.java
+++ app/src/main/java/taffer/statcounter/MainActivity.java
@@ -16,5 +16,11 @@ public class MainActivity extends AppCompatActivity {
         }
         setContentView(R.layout.activity_main);
     }
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        outState.putSerializable("BUILDER", this.gb);
+        super.onSaveInstanceState(outState);
+    }
+
 
 }
