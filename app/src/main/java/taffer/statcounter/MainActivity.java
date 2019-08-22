93e374ebcff524783f5a24e98eb6ae8af762f529
simon <simon.tenggren@gmail.com>
2019-08-22 13:20:43 +0200

    work

--- app/src/main/java/taffer/statcounter/MainActivity.java
+++ app/src/main/java/taffer/statcounter/MainActivity.java
@@ -2,7 +2,6 @@ package taffer.statcounter;
 
 
 import android.support.v4.app.FragmentTransaction;
-
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -10,15 +9,19 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
-import android.view.MotionEvent;
 import android.view.View;
+import android.widget.RadioGroup;
+import android.widget.Toast;
+
 import taffer.statcounter.Fragments.GamemodeFragment;
+import taffer.statcounter.Fragments.PlayerFragment;
 import taffer.statcounter.Model.GameBuilder;
 
 public class MainActivity extends AppCompatActivity {
     private GameBuilder gb;
     private int step = 0;
     private ActionBar actionBar;
+    private Toast toast;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,7 +37,7 @@ public class MainActivity extends AppCompatActivity {
 
         setContentView(R.layout.activity_main);
         FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
-        ft.add(R.id.fragment_frame, new GamemodeFragment()).commit();
+        ft.add(R.id.fragment_frame, new PlayerFragment()).commit();
         //this.disableShowHideAnimation(this.actionBar);
         // TODO: ADD COOL TRANSITION !!
     }
@@ -55,8 +58,58 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void nextStage(View v){
-        this.step++;
-        //setLayout();
+        int initialStep = this.step;
+
+        switch(this.step){
+            case 0:
+                this.step = this.chosePlayerCount();
+                break;
+        }
+
+        Log.d("Ini, curr", initialStep + " | " + this.step);
+        if(this.step != initialStep){
+            this.changeStep();
+        }
+    }
+
+    private int chosePlayerCount(){
+        RadioGroup rGroup = findViewById(R.id.rGroupPlayers);
+        int id = rGroup.getCheckedRadioButtonId();
+
+        Log.e("ID", id + "");
+        if(id == -1){
+            displayToast("Please select one of the options.");
+            return 0;
+        } else {
+            if(id == R.id.rBtnOne) {
+                this.gb = gb.setNoOfPlayers(1);
+            }else{
+                this.gb = gb.setNoOfPlayers(2);
+            }
+
+            return 1;
+        }
+    }
+    private void changeStep(){
+        FragmentTransaction ft;
+        switch (this.step){
+            case 0:
+                 ft = getSupportFragmentManager().beginTransaction();
+                ft.add(R.id.fragment_frame, new PlayerFragment()).commit();
+                break;
+            case 1:
+                ft = getSupportFragmentManager().beginTransaction();
+                ft.add(R.id.fragment_frame, new GamemodeFragment()).commit();
+                break;
+        }
+    }
+
+    private void displayToast(String s){
+            if(this.toast != null) {
+                this.toast.cancel();
+            }
+            this.toast = Toast.makeText(this, s, Toast.LENGTH_LONG);
+            this.toast.show();
     }
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
@@ -82,5 +135,14 @@ public class MainActivity extends AppCompatActivity {
     public void showBar(View v){
         findViewById(R.id.floatingActionButton2).setVisibility(View.INVISIBLE);
         this.actionBar.show();
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(this.step == 0){
+            return;
+        }
+        this.step--;
+        changeStep();
     }
 }
