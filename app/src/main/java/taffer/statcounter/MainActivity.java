8b36cac6399b43dd2785ac13acd3e715875c28c9
simon <simon.tenggren@gmail.com>
2019-08-23 12:30:11 +0200

    Animations and onClicks

--- app/src/main/java/taffer/statcounter/MainActivity.java
+++ app/src/main/java/taffer/statcounter/MainActivity.java
@@ -1,8 +1,16 @@
 package taffer.statcounter;
 
-
+import android.animation.AnimatorSet;
+import android.animation.ObjectAnimator;
+import android.animation.PropertyValuesHolder;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.content.Intent;
+import android.widget.ImageView;
+
+import taffer.statcounter.Model.Game;
+import taffer.statcounter.Model.GameBuilder;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +18,47 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ImageView iv = (ImageView) findViewById(R.id.ivLogo);
+
+        ObjectAnimator animator = ObjectAnimator.ofPropertyValuesHolder(
+                iv,
+                PropertyValuesHolder.ofFloat("scaleX", 1.1f),
+                PropertyValuesHolder.ofFloat("scaleY", 1.1f));
+        animator.setDuration(500);
+        animator.setRepeatCount(ObjectAnimator.INFINITE);
+        animator.setRepeatMode(ObjectAnimator.REVERSE);
+
+        ObjectAnimator imageViewObjectAnimator = ObjectAnimator.ofFloat(iv ,
+                "rotation", 0f, 360f);
+        imageViewObjectAnimator.setDuration(5000);
+        imageViewObjectAnimator.setRepeatCount(ObjectAnimator.INFINITE);
+        imageViewObjectAnimator.setRepeatMode(ObjectAnimator.RESTART);
+        // Starts animations
+        AnimatorSet animatorSet = new AnimatorSet();
+        animatorSet.playTogether(animator,imageViewObjectAnimator);
+
+        animatorSet.start();
+
+    }
+
+    public void newGame(View v){
+        Intent i = new Intent(this, BuilderActivity.class);
+        startActivity(i);
+    }
+
+    public void quickGame(View v){
+        // TODO: ADD DEFAULT 1 AND 2 PLAYER NAMES AND COLORS FOR QUICK GAME
+        Game g = new GameBuilder().setNoOfPlayers(1).setGameMode("Constructed").addPlayer("Player 1", "someColor").build();
+        Intent i = new Intent(this, GameActivity.class);
+        i.putExtra("GAME", g);
+        startActivity(i);
+    }
+
+    public void quick2PlayerGame(View v){
+        // TODO: ADD DEFAULT SETTINGS FOR QUICK GAME
+        Game g = new GameBuilder().setNoOfPlayers(2).setGameMode("Constructed").addPlayer("Player 1", "someColor").addPlayer("Player 2", "anotherColor").build();
+        Intent i = new Intent(this, GameActivity.class);
+        i.putExtra("GAME", g);
+        startActivity(i);
     }
 }
--- app/src/main/java/taffer/statcounter/Model/RandomHelper.java
+++ app/src/main/java/taffer/statcounter/Model/RandomHelper.java
@@ -1,8 +1,9 @@
 package taffer.statcounter.Model;
 
+import java.io.Serializable;
 import java.util.Random;
 
-public class RandomHelper {
+public class RandomHelper implements Serializable {
     private Random rand = new Random();
 
     public int rollDie(){
