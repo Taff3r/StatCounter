9e229a6a3707eb852dbabc3ab7a5d60c3ffda393
Simon Tenggren <simon.tenggren@gmail.com>
2019-08-28 23:07:37 +0200

    removed all instances of Log

--- app/src/main/java/taffer/statcounter/GameActivity.java
+++ app/src/main/java/taffer/statcounter/GameActivity.java
@@ -12,22 +12,17 @@ import android.media.MediaPlayer;
 import android.net.Uri;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
-import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.FrameLayout;
 import android.widget.TextView;
-
-import org.w3c.dom.Text;
 
 import taffer.statcounter.Fragments.Game1Fragment;
 import taffer.statcounter.Fragments.Game2Fragment;
